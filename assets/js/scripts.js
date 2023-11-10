41d5d1237fc1d887228a8a20b666cc2e2131cfed
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-11-10 12:06:03 +0100

    feat: add website

---
+++ assets/js/scripts.js
@@ -0,0 +1,10 @@
+function changeNav() {
+  var nav = document.getElementById("main-nav");
+  nav.classList.toggle("nav-active");
+  var burger = document.getElementById("burger-menu");
+  burger.classList.toggle("burger-highlight");
+}
+
+function incrementCounters() {
+  
+}
